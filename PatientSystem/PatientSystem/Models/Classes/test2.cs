6bef064e9febd81b7f320dc152cbad83e7b0fc70
Firas Quzaih <f.qzieh@gmail.com>
2021-11-09 10:57:07 +0200

    craete class test2 for project

---
+++ PatientSystem/PatientSystem/Models/Classes/test2.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PatientSystem.Models.Classes
+{
+    public class test2
+    {
+    }
+}
